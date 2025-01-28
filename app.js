f1b3dda7fdf6edf84d5a95840a8455a14e8f7af4
Vinicius Leme <vinicius.leme00@gmail.com>
2025-01-28 14:53:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Ola jinglebobs, Bem vindo ao jogo do número secreto');
 let numeroSecreto = 13;
-prompt('Escolha um número entre 1 e 15');
+let chute = prompt('Escolha um número entre 1 e 15');
+
+if (chute == numeroSecreto) {
+    console.log ('Parabéns, você acertou o número secreto (13)');
+}
