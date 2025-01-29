ffd66ab6d4a69be2dc56eb63e001a385aa72d9c1
Vinicius Leme <vinicius.leme00@gmail.com>
2025-01-29 13:59:36 -0300

    edit
    Troquei o número na linha 2
    adicionei comentario na linha 5 e 6
    e adicionei o comando parseInt para o  comando prompt para que o chute não seja confundido com uma string

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 alert('Ola jinglebobs, Bem vindo ao jogo do número secreto');
-let numeroSecreto = 13;
+let numeroSecreto = 8;
 let chute = prompt('Escolha um número entre 1 e 15');
+
+// convertendo o prompt chute para número
+chute = parseInt(chute); // Também pode ser usado Number(chute); para números decimal
 
 if (chute == numeroSecreto) {
     alert (`Parabéns, você acertou o número secreto! ${numeroSecreto}`);
 } else {
-    alert('Você errou o número secreto, tente novamente!');
+    alert(`Você errou o número secreto, o número secreto era ${numeroSecreto}, mas você escolheu ${chute}!`);
 }
