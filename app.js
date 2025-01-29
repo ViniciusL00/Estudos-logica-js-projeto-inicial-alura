5ef0ec690075a5f8c0852a5d40c9d521222ce35f
Vinicius Leme <vinicius.leme00@gmail.com>
2025-01-29 14:24:48 -0300

    edit
    Coloquei console.log na linha 4 e 5 para dizer o valor do Chute e para dizer se o resultado é verdadeiro ou falso
    também coloquei console.log na linha 13 para dizer o valor do numerosecreto

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 alert('Ola jinglebobs, Bem vindo ao jogo do número secreto');
 let numeroSecreto = 8;
-console.log ('o número secreto é 8');
 let chute = prompt('Escolha um número entre 1 e 15');
+console.log ('valor do chute', chute);
+console.log ('Resultado da comparação', chute == numeroSecreto);
 
 // convertendo o prompt chute para número
 chute = parseInt(chute); // Também pode ser usado Number(chute); para números decimal
@@ -9,5 +10,6 @@ chute = parseInt(chute); // Também pode ser usado Number(chute); para números 
 if (chute == numeroSecreto) {
     alert (`Parabéns, você acertou o número secreto! ${numeroSecreto}`);
 } else {
+    console.log('Valor do número secreto', numeroSecreto);
     alert(`Você errou o número secreto, o número secreto era ${numeroSecreto}, mas você escolheu ${chute}!`);
 }
