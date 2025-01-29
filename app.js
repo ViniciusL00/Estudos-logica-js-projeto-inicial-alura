6a706939307202d2939f716d170b9493cb13c0d1
Vinicius Leme <vinicius.leme00@gmail.com>
2025-01-29 12:38:42 -0300

    Edit
    Retirei o console.log da linha 6
    adicionei 2 alert e else entre a linha 6 até a 8

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 13;
 let chute = prompt('Escolha um número entre 1 e 15');
 
 if (chute == numeroSecreto) {
-    console.log ('Parabéns, você acertou o número secreto (13)');
+    alert ('Parabéns, você acertou o número secreto!');
+} else {
+    alert('Você errou o número secreto, tente novamente!');
 }
