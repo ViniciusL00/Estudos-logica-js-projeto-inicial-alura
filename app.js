fc8ce75c15415049916695c15b45e547d49ee3aa
Vinicius Leme <vinicius.leme00@gmail.com>
2025-01-29 14:44:15 -0300

    Update app.js
    Desativei o console.log para que não possa ser visto na pagina e só no codigo

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 alert('Ola jinglebobs, Bem vindo ao jogo do número secreto');
 let numeroSecreto = 8;
 let chute = prompt('Escolha um número entre 1 e 15');
-console.log ('valor do chute', chute);
-console.log ('Resultado da comparação', chute == numeroSecreto);
+
+// Desabilitando a funcão console.log
+console.log = function() {}; // isso faz com que o console.log não mostre nada
+
+console.log('valor do chute', chute);
+console.log('Resultado da comparação', chute == numeroSecreto);
+console.log('Valor do número secreto', numeroSecreto);
 
 // convertendo o prompt chute para número
 chute = parseInt(chute); // Também pode ser usado Number(chute); para números decimal
@@ -10,6 +15,5 @@ chute = parseInt(chute); // Também pode ser usado Number(chute); para números 
 if (chute == numeroSecreto) {
     alert (`Parabéns, você acertou o número secreto! ${numeroSecreto}`);
 } else {
-    console.log('Valor do número secreto', numeroSecreto);
     alert(`Você errou o número secreto, o número secreto era ${numeroSecreto}, mas você escolheu ${chute}!`);
 }
