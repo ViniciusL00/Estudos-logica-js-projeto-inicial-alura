2f8847fc08e53b5a149693310ac66244e1910cb1
Vinicius Leme <vinicius.leme00@gmail.com>
2025-01-29 14:45:28 -0300

    Update app.js
    Alguns comentarios na linha 8, 9 e 10

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ let numeroSecreto = 8;
 let chute = prompt('Escolha um número entre 1 e 15');
 
 // Desabilitando a funcão console.log
-console.log = function() {}; // isso faz com que o console.log não mostre nada
+console.log = function() {}; // Isso faz com que o console.log não mostre nada
 
-console.log('valor do chute', chute);
-console.log('Resultado da comparação', chute == numeroSecreto);
-console.log('Valor do número secreto', numeroSecreto);
+console.log('valor do chute', chute); // Isso não aparecerá
+console.log('Resultado da comparação', chute == numeroSecreto); // Isso não aparecerá
+console.log('Valor do número secreto', numeroSecreto); // Isso não aparecerá
 
 // convertendo o prompt chute para número
 chute = parseInt(chute); // Também pode ser usado Number(chute); para números decimal
